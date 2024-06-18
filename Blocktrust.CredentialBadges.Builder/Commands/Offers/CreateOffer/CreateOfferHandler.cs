ce054837e423be53398928da7468d621caff2e32
ndigirigijohn <jontejonhn@gmail.com>
2024-06-18 08:19:23 +0300

    updated the create offer request to extend CreateIssueCredentialRecordRequest from IdentusApi and handler to use identus api

--- Blocktrust.CredentialBadges.Builder/Commands/Offers/CreateOffer/CreateOfferHandler.cs
+++ Blocktrust.CredentialBadges.Builder/Commands/Offers/CreateOffer/CreateOfferHandler.cs
@@ -1,5 +1,8 @@
 using FluentResults;
 using MediatR;
+using Microsoft.Extensions.Options;
+using Blocktrust.CredentialBadges.Builder.Common;
+using Blocktrust.CredentialBadges.IdentusClientApi;
 
 namespace Blocktrust.CredentialBadges.Builder.Commands.Offers.CreateOffer;
 
@@ -7,35 +10,29 @@ public class CreateOfferHandler : IRequestHandler<CreateOfferRequest, Result<str
 {
     private readonly HttpClient _httpClient;
     private readonly ILogger<CreateOfferHandler> _logger;
- 
-    public CreateOfferHandler(IHttpClientFactory httpClientFactory, ILogger<CreateOfferHandler> logger)
+    private readonly AppSettings _appSettings;
+
+    public CreateOfferHandler(IHttpClientFactory httpClientFactory, ILogger<CreateOfferHandler> logger, IOptions<AppSettings> appSettings)
     {
-        _httpClient = httpClientFactory.CreateClient();
+        _httpClient = httpClientFactory.CreateClient("IdentusAgents");
+        _appSettings = appSettings.Value;
         _logger = logger;
     }
 
     public async Task<Result<string>> Handle(CreateOfferRequest request, CancellationToken cancellationToken)
     {
+        _httpClient.DefaultRequestHeaders.Add("apiKey", _appSettings.Agent1ApiKey);
+        var identusClient = new IdentusClient(_httpClient);
+
+        identusClient.BaseUrl = _appSettings.Agent1BaseUrl; // or Agent2BaseUrl 
+
         try
         {
-            // Add the apiKey to the request headers
-            _httpClient.DefaultRequestHeaders.Add("apiKey", request.ApiKey);
-
-            var response = await _httpClient.PostAsJsonAsync("http://212.124.52.36:35412/cloud-agent/issue-credentials/credential-offers", request, cancellationToken);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var responseBody = await response.Content.ReadAsStringAsync(cancellationToken);
-                
-                
-                
-                return Result.Ok(responseBody);
-            }
-
-            var error = await response.Content.ReadAsStringAsync(cancellationToken);
-            return Result.Fail(error);
+            var reqs = _httpClient.DefaultRequestHeaders.ToString();
+            var response = await identusClient.CreateCredentialOfferAsync(request, cancellationToken);
+            return Result.Ok(response.RecordId);
         }
-        catch (Exception ex)
+        catch (ApiException ex)
         {
             _logger.LogError(ex, "Error creating offer");
             return Result.Fail(ex.Message);
