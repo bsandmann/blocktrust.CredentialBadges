c00226e3c5b2a93b0e416808ac1d6826c99f4e13
Björn Sandmann <sandmann@codedata.solutions>
2024-06-19 13:03:15 +0200

    Clipcoard btn

---
+++ Blocktrust.CredentialBadges.Core/Services/Clipboard/ClipboardService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+public class ClipboardService
+{
+    private IJSRuntime JSRuntime { get; set; }
+
+    public ClipboardService(IJSRuntime jsRuntime)
+    {
+        JSRuntime = jsRuntime;
+    }
+
+    public async Task CopyTextToClipboard(string? text)
+    {
+        if (!string.IsNullOrEmpty(text))
+        {
+            await JSRuntime.InvokeVoidAsync("clipboardCopy.copyText", text);
+        }
+    }
+}
