7e5d610d50adc1d4bcc35d487510c00fa225870a
Björn Sandmann <sandmann@codedata.solutions>
2024-06-27 09:29:32 +0200

    Added Crypot libs

---
+++ Blocktrust.CredentialBadges.Core/Crypto/CryptoConstants.cs
@@ -0,0 +1,8 @@
+namespace Blocktrust.CredentialBadges.Core.Crypto;
+
+public class CryptoConstants
+{
+    public const string Secp256k1CurveName = "secp256k1";
+    public const string Ed25519CurveName = "edd25519";
+    public const string X25519CurveName = "x25519";
+}
