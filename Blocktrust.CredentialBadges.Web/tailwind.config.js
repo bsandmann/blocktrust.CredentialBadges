34c1aafa3c6b4945b889c56bd6a73d13aec00132
ndigirigijohn <jontejonhn@gmail.com>
2024-07-04 17:29:49 +0300

    credential badges templates

--- Blocktrust.CredentialBadges.Web/tailwind.config.js
+++ Blocktrust.CredentialBadges.Web/tailwind.config.js
@@ -19,7 +19,8 @@ module.exports = {
                 'sans': ['museo-sans', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
                 'museo': ['museo', 'serif'],
                 'museo-sans': ['museo-sans', 'sans-serif'],
-                'code': ['source-code-variable', 'sans-serif']
+                'code': ['source-code-variable', 'sans-serif'],
+                'poppins': ['Poppins', 'sans-serif'],
             }
         }
     },
