40e8a3589f56313f7bf98721ee598b3008ea4b58
ndigirigijohn <jontejonhn@gmail.com>
2024-07-07 15:54:04 +0300

    Fixed styles and created new navigation

---
+++ Blocktrust.CredentialBadges.Web/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+// wwwroot/js/site.js
+
+function initMobileMenu() {
+    const btn = document.querySelector("button.mobile-menu-button");
+    const menu = document.querySelector(".mobile-menu");
+
+    btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+}
