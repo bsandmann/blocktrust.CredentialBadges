18474a9f72ae55032451e7cd6ff40a581264dccf
Björn Sandmann <sandmann@codedata.solutions>
2024-08-07 16:46:43 +0200

    Sample on how to sign and verify data

--- Blocktrust.CredentialBadges.Web.Tests/Verification/VerifiactionTests.cs
+++ Blocktrust.CredentialBadges.Web.Tests/Verification/VerifiactionTests.cs
@@ -5,6 +5,8 @@ using Core.Commands.CheckSignature;
 using Core.Commands.CheckTrustRegistry;
 using Core.Commands.VerifyOpenBadge;
 using Core.Common;
+using Core.Crypto;
+using Core.Prism;
 using Xunit;
 using FluentResults.Extensions.FluentAssertions;
 using MediatR;
@@ -44,5 +46,23 @@ public partial class TestSetup
 
         // Assert
         verifivationResult.Should().BeSuccess();
+    }
+    
+    [Fact]
+    public void BouncyCastle_is_able_to_sign_and_verify_message_as_expected()
+    {
+        // Arrange
+        var privateKeyHex = "c1edddfe6b0dfdcd7dd5af7bac2c44bd5361b6e9516985d5fd1d971d91520628";
+        var publicKeyHex = "04b07372dd8077224e8cdede04908627dbc7749c8c148e21204ab5863bce9fa0a5063fd7e55baee446ee97c979f37507ae3ce21b4c261075398d54382e3ada2e2f";
+        var testMessage = "abcdefgh123456";
+        var testMessageBytes = PrismEncoding.Utf8StringToByteArray(testMessage);
+        var ecService = new EcServiceBouncyCastle();
+
+        // Act
+        var signature = ecService.SignData(testMessageBytes, PrismEncoding.HexToByteArray(privateKeyHex));
+        var validation = ecService.VerifyData(testMessageBytes, signature, PrismEncoding.HexToByteArray(publicKeyHex));
+
+        // Assert
+        Assert.True(validation);
     }
 }
