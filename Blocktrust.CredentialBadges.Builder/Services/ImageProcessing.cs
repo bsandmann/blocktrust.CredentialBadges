7e09759d47934a3d36385afed4e2a12b6390b97f
ndigirigijohn <jontejonhn@gmail.com>
2024-09-25 12:43:36 +0300

    rolled back to using imagesharp

--- Blocktrust.CredentialBadges.Builder/Services/ImageProcessing.cs
+++ Blocktrust.CredentialBadges.Builder/Services/ImageProcessing.cs
@@ -1,80 +1,38 @@
-using Emgu.CV;
-using Emgu.CV.CvEnum;
-using Emgu.CV.Structure;
-using System.Drawing;
-using Emgu.CV.Util;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.Formats.Jpeg;
 
 namespace Blocktrust.CredentialBadges.Builder.Services;
 
 public class ImageProcessingService
 {
-    private readonly ILogger<ImageProcessingService> _logger;
-
-    public ImageProcessingService(ILogger<ImageProcessingService> logger)
+    public static async Task<string> ProcessImageAsync(Stream imageStream, string contentType, int targetWidth, int targetHeight)
     {
-        _logger = logger;
-    }
-
-    public async Task<string> ProcessImageAsync(Stream imageStream, string contentType, int targetWidth, int targetHeight)
-    {
-        try
-        {
-            using var ms = new MemoryStream();
-            await imageStream.CopyToAsync(ms);
-            ms.Position = 0;
-
-            using var originalImage = new Mat();
-            CvInvoke.Imdecode(ms.ToArray(), ImreadModes.Unchanged, originalImage);
-
-            if (originalImage.IsEmpty)
+        using var image = await Image.LoadAsync(imageStream);
+            
+        // Resize and pad the image
+        image.Mutate(x => x
+            .Resize(new ResizeOptions
             {
-                throw new ArgumentException("Unable to decode the image.");
-            }
+                Size = new Size(targetWidth, targetHeight),
+                Mode = ResizeMode.Pad
+            })
+        );
 
-            var aspectRatio = (double)originalImage.Width / originalImage.Height;
-            var newWidth = targetWidth;
-            var newHeight = targetHeight;
-
-            if (aspectRatio > 1)
-            {
-                newHeight = (int)(targetHeight / aspectRatio);
-            }
-            else
-            {
-                newWidth = (int)(targetWidth * aspectRatio);
-            }
-
-            using var resizedImage = new Mat();
-            CvInvoke.Resize(originalImage, resizedImage, new Size(newWidth, newHeight), 0, 0, Inter.Linear);
-
-            using var paddedImage = new Mat(new Size(targetWidth, targetHeight), DepthType.Cv8U, originalImage.NumberOfChannels);
-            paddedImage.SetTo(new MCvScalar(0, 0, 0, 0)); // Transparent background
-
-            var roi = new Rectangle((targetWidth - newWidth) / 2, (targetHeight - newHeight) / 2, newWidth, newHeight);
-            using var roiMat = new Mat(paddedImage, roi);
-            resizedImage.CopyTo(roiMat);
-
-            using var encodedImage = new VectorOfByte();
-            CvInvoke.Imencode(GetFileExtension(contentType), paddedImage, encodedImage);
-
-            var base64 = Convert.ToBase64String(encodedImage.ToArray());
-            return $"data:{contentType};base64,{base64}";
-        }
-        catch (Exception ex)
+        // Determine the output format
+        IImageEncoder encoder = contentType switch
         {
-            _logger.LogError(ex, "Error processing image with Emgu CV.");
-            throw;
-        }
-    }
-
-    private string GetFileExtension(string contentType)
-    {
-        return contentType switch
-        {
-            "image/png" => ".png",
-            "image/jpeg" => ".jpg",
-            "image/gif" => ".gif",
+            "image/png" => new PngEncoder(),
+            "image/jpeg" => new JpegEncoder(),
+            "image/gif" => new GifEncoder(),
             _ => throw new ArgumentException("Unsupported image format")
         };
+
+        using var ms = new MemoryStream();
+        await image.SaveAsync(ms, encoder);
+        return Convert.ToBase64String(ms.ToArray());
     }
 }
