120116f51159ab5db3ade16a2f4cc9b722924466
ndigirigijohn <jontejonhn@gmail.com>
2024-09-30 11:08:48 +0300

    handling unusable connectes and credential and improving new did and connections page

--- Blocktrust.CredentialBadges.Builder/Commands/BuilderCredentials/GetBuilderCrdentialByUserId/GetBuilderCrdentialByUserIdRequest.cs
+++ Blocktrust.CredentialBadges.Builder/Commands/BuilderCredentials/GetBuilderCrdentialByUserId/GetBuilderCrdentialByUserIdRequest.cs
@@ -1,10 +1,22 @@
 namespace Blocktrust.CredentialBadges.Builder.Commands.BuilderCredentials.GetBuilderCrdentialByUserId;
 
-using Blocktrust.CredentialBadges.Builder.Domain;
+using Domain;
 using FluentResults;
 using MediatR;
 
 public class GetBuilderCredentialsByUserIdRequest : IRequest<Result<List<BuilderCredential>>>
 {
     public string UserId { get; set; }
+    public string SubjectDid { get; set; }
+
+    public GetBuilderCredentialsByUserIdRequest(string userId)
+    {
+        UserId = userId;
+    }
+
+    public GetBuilderCredentialsByUserIdRequest(string userId, string subjectDid)
+    {
+        UserId = userId;
+        SubjectDid = subjectDid;
+    }
 }
