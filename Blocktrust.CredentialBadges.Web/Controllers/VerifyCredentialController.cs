c149d8fba8efdff4b48db624c1e3fdce22a82bcd
bjoern <sandmann@codedata.solutions>
2025-03-06 17:58:50 +0100

    Verifier for Endorsement now displaying correct claims

--- Blocktrust.CredentialBadges.Web/Controllers/VerifyCredentialController.cs
+++ Blocktrust.CredentialBadges.Web/Controllers/VerifyCredentialController.cs
@@ -25,7 +25,7 @@ public class VerifyCredentialController : ControllerBase
     {
         // Get the credential from the database by calling the mediator with GetVerifiedCredentialByIdRequest
         var credentialResult = await _mediator.Send(new GetVerifiedCredentialByIdRequest(id));
-        
+
         if (credentialResult.IsFailed)
         {
             return NotFound(new { Message = "Credential not found" });
@@ -34,72 +34,134 @@ public class VerifyCredentialController : ControllerBase
         var credential = credentialResult.Value;
 
         // Deserialize the credential to an AchievementCredential
-        
+
         var parserResult = CredentialParser.Parse(credential.Credential);
         if (parserResult.IsFailed)
         {
             return BadRequest(new { Message = "Invalid credential", Details = parserResult.Errors });
         }
 
-        AchievementCredential achievementCredential = parserResult.Value as AchievementCredential;
-
-
-        // Call the verification command to re-verify the credential
-        
-        if (achievementCredential is null)
+        if (parserResult.Value is AchievementCredential)
         {
-            return BadRequest(new { Message = "Invalid credential" });
-        }
-        
-        var verifyResult = await _mediator.Send(new VerifyOpenBadgeRequest(achievementCredential));
+            AchievementCredential achievementCredential = parserResult.Value as AchievementCredential;
 
-        if (verifyResult.IsFailed)
-        {
-            return BadRequest(new { Message = "Verification failed", Details = verifyResult.Errors });
-        }
-
-        var verifyResponse = verifyResult.Value;
-  
-        VerificationResponse response = new VerificationResponse
-        {
-            Id = id,
-            Status = EVerificationStatus.Verified,
-            Name = achievementCredential.CredentialSubject?.Achievement?.Name ?? achievementCredential.CredentialSubject?.Id?.ToString() ?? "Unknown",
-            Description = achievementCredential.CredentialSubject?.Achievement?.Description ?? "",
-            Claims = achievementCredential.CredentialSubject?.Claims,
-            Issuer = achievementCredential.Issuer?.Id?.ToString() ?? "Unknown Issuer",
-            Image = achievementCredential.CredentialSubject?.Achievement?.Image?.Id!=null?achievementCredential.CredentialSubject.Achievement.Image.Id.ToString():"",
-            VerificationChecks = new VerifyOpenBadgeResponse
+            if (achievementCredential is null)
             {
-                CheckedOn = DateTime.UtcNow,
-                SignatureIsValid = verifyResponse.SignatureIsValid,
-                CredentialIsNotRevoked = verifyResponse.CredentialIsNotRevoked,
-                CredentialIsNotExpired = verifyResponse.CredentialIsNotExpired,
-                CredentialIssuanceDateIsNotInFuture = verifyResponse.CredentialIssuanceDateIsNotInFuture
+                return BadRequest(new { Message = "Invalid credential" });
             }
-         
-        };
-        
-        if(verifyResponse.CredentialIsNotExpired == false)
-        {
-            response.Status = EVerificationStatus.Expired;
+
+            var verifyResult = await _mediator.Send(new VerifyOpenBadgeRequest(achievementCredential));
+
+            if (verifyResult.IsFailed)
+            {
+                return BadRequest(new { Message = "Verification failed", Details = verifyResult.Errors });
+            }
+
+            var verifyResponse = verifyResult.Value;
+
+            VerificationResponse response = new VerificationResponse
+            {
+                Id = id,
+                SubjectId = achievementCredential.CredentialSubject.Id.ToString(),
+                SubjectName = achievementCredential.CredentialSubject.Identifier,
+                Status = EVerificationStatus.Verified,
+                Name = achievementCredential.CredentialSubject?.Achievement?.Name ?? achievementCredential.CredentialSubject?.Id?.ToString() ?? "Unknown",
+                Description = achievementCredential.CredentialSubject?.Achievement?.Description ?? "",
+                Claims = achievementCredential.CredentialSubject?.Claims,
+                Issuer = achievementCredential.Issuer?.Id?.ToString() ?? "Unknown Issuer",
+                Image = achievementCredential.CredentialSubject?.Achievement?.Image?.Id != null ? achievementCredential.CredentialSubject.Achievement.Image.Id.ToString() : "",
+                VerificationChecks = new VerifyOpenBadgeResponse
+                {
+                    CheckedOn = DateTime.UtcNow,
+                    SignatureIsValid = verifyResponse.SignatureIsValid,
+                    CredentialIsNotRevoked = verifyResponse.CredentialIsNotRevoked,
+                    CredentialIsNotExpired = verifyResponse.CredentialIsNotExpired,
+                    CredentialIssuanceDateIsNotInFuture = verifyResponse.CredentialIssuanceDateIsNotInFuture
+                }
+            };
+
+            if (verifyResponse.CredentialIsNotExpired == false)
+            {
+                response.Status = EVerificationStatus.Expired;
+            }
+            else if (verifyResponse.CredentialIsNotRevoked == false)
+            {
+                response.Status = EVerificationStatus.Revoked;
+            }
+
+            else if (verifyResponse.SignatureIsValid == false)
+            {
+                response.Status = EVerificationStatus.Invalid;
+            }
+            else if (verifyResponse.CredentialIssuanceDateIsNotInFuture == false)
+            {
+                response.Status = EVerificationStatus.NotDue;
+            }
+
+            // Return the id and status of the verification
+            return Ok(response);
         }
-        else if(verifyResponse.CredentialIsNotRevoked == false)
+        else if (parserResult.Value is EndorsementCredential)
         {
-            response.Status = EVerificationStatus.Revoked;
+              EndorsementCredential endorsementCredential = parserResult.Value as EndorsementCredential;
+
+            if (endorsementCredential is null)
+            {
+                return BadRequest(new { Message = "Invalid credential" });
+            }
+
+            var verifyResult = await _mediator.Send(new VerifyOpenBadgeRequest(endorsementCredential));
+
+            if (verifyResult.IsFailed)
+            {
+                return BadRequest(new { Message = "Verification failed", Details = verifyResult.Errors });
+            }
+
+            var verifyResponse = verifyResult.Value;
+
+            VerificationResponse response = new VerificationResponse
+            {
+                Id = id,
+                SubjectId = endorsementCredential.CredentialSubject.Id.ToString(),
+                SubjectName = endorsementCredential.CredentialSubject.Claims.TryGetValue("identifier", out var identifier) ? identifier : "",
+                Status = EVerificationStatus.Verified,
+                Name = endorsementCredential.CredentialSubject.Claims.TryGetValue("name", out var name) ? name : null ?? endorsementCredential.CredentialSubject?.Id?.ToString() ?? "Unknown",
+                Description =endorsementCredential.CredentialSubject.Claims.TryGetValue("description", out var description) ? description : "",
+                Claims = endorsementCredential.CredentialSubject?.Claims.Where(p=>p.Key.ToLowerInvariant()!="name" && p.Key.ToLowerInvariant()!="description" && p.Key.ToLowerInvariant()!="indentifier").ToDictionary(),
+                Issuer = endorsementCredential.Issuer?.Id?.ToString() ?? "Unknown Issuer",
+                Image = "",
+                VerificationChecks = new VerifyOpenBadgeResponse
+                {
+                    CheckedOn = DateTime.UtcNow,
+                    SignatureIsValid = verifyResponse.SignatureIsValid,
+                    CredentialIsNotRevoked = verifyResponse.CredentialIsNotRevoked,
+                    CredentialIsNotExpired = verifyResponse.CredentialIsNotExpired,
+                    CredentialIssuanceDateIsNotInFuture = verifyResponse.CredentialIssuanceDateIsNotInFuture
+                }
+            };
+
+            if (verifyResponse.CredentialIsNotExpired == false)
+            {
+                response.Status = EVerificationStatus.Expired;
+            }
+            else if (verifyResponse.CredentialIsNotRevoked == false)
+            {
+                response.Status = EVerificationStatus.Revoked;
+            }
+
+            else if (verifyResponse.SignatureIsValid == false)
+            {
+                response.Status = EVerificationStatus.Invalid;
+            }
+            else if (verifyResponse.CredentialIssuanceDateIsNotInFuture == false)
+            {
+                response.Status = EVerificationStatus.NotDue;
+            }
+
+            // Return the id and status of the verification
+            return Ok(response);
         }
-     
-        else if(verifyResponse.SignatureIsValid == false)
-        {
-            response.Status = EVerificationStatus.Invalid;
-        }
-        else if (verifyResponse.CredentialIssuanceDateIsNotInFuture == false)
-        {
-            response.Status = EVerificationStatus.NotDue;
-        }
-        // Return the id and status of the verification
-        return Ok(response);
+
+        return BadRequest("Invalid credential");
     }
 }
-
-
--- Blocktrust.CredentialBadges.Web/Domain/VerificationResponse.cs
+++ Blocktrust.CredentialBadges.Web/Domain/VerificationResponse.cs
@@ -7,17 +7,19 @@ public class VerificationResponse
 {
     public Guid Id { get; set; }
     public EVerificationStatus Status { get; set; }
-    
+
     public string Name { get; set; }
-    
+
     public string Description { get; set; }
     public string Issuer { get; set; }
     public string Domain { get; set; }
 
+    public string SubjectId { get; set; }
+    public string? SubjectName { get; set; }
     public Dictionary<string,string>? Claims { get; set; }
-    
+
     public string? Image { get; set; }
-    
+
     public VerifyOpenBadgeResponse VerificationChecks { get; set; }
 }
 
