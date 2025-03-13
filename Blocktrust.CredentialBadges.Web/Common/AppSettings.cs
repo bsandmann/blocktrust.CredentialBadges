873bbdc5eeda4e3941f4540237c1d2f0c1c98520
bjoern <sandmann@codedata.solutions>
2025-03-13 19:20:42 +0100

    moved hardcoded url to appsettings.json

--- Blocktrust.CredentialBadges.Web/Common/AppSettings.cs
+++ Blocktrust.CredentialBadges.Web/Common/AppSettings.cs
@@ -3,10 +3,16 @@ namespace Blocktrust.CredentialBadges.Web.Common;
 public class AppSettings
 {
     public PrismDidSettings PrismDid { get; set; }
+    public BadgeSettings Badge { get; set; }
 }
 
 public class PrismDidSettings
 {
     public string BaseUrl { get; set; }
     public string DefaultLedger { get; set; }
+}
+
+public class BadgeSettings
+{
+    public string LoaderScriptUrl { get; set; }
 }
