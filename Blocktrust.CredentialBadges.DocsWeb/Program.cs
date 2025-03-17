09a41b85d6dfb8f1ae1cfbdbca2d601d79aecb54
bjoern <sandmann@codedata.solutions>
2025-03-17 19:14:40 +0100

    refinements in docu, WIP

--- Blocktrust.CredentialBadges.DocsWeb/Program.cs
+++ Blocktrust.CredentialBadges.DocsWeb/Program.cs
@@ -1,9 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.WebHost.ConfigureKestrel(options =>
-// {
-//     options.ListenAnyIP(8080); // Listen on port 8080 for HTTP - HTTPS is handled by Traefik enterily! No https ports or redirection here!
-// });
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080); // Listen on port 8080 for HTTP - HTTPS is handled by Traefik enterily! No https ports or redirection here!
+});
 
 
 var app = builder.Build();
