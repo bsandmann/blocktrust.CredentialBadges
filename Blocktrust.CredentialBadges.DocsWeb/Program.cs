4fea25af6b75a274f12451ad32d93e4d9cf9c9c8
bjoern <sandmann@codedata.solutions>
2025-03-17 18:42:30 +0100

    added docs

--- Blocktrust.CredentialBadges.DocsWeb/Program.cs
+++ Blocktrust.CredentialBadges.DocsWeb/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.WebHost.ConfigureKestrel(options =>
+// {
+//     options.ListenAnyIP(8080); // Listen on port 8080 for HTTP - HTTPS is handled by Traefik enterily! No https ports or redirection here!
+// });
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Enable serving default files like index.html
+app.UseDefaultFiles();
+
+// Enable serving static files from wwwroot
+app.UseStaticFiles();
 
 app.Run();
